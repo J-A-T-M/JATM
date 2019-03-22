7bf92b1936529d225aa7b1518d3198ed0e77628c
limbermuffin <limbermuffin@gmail.com>
2019-03-21 20:11:31 -0700

    forgot to delete floor

--- OpenGLTest/MainScene.cpp
+++ OpenGLTest/MainScene.cpp
@@ -25,7 +25,7 @@ MainScene::MainScene(bool isServer, std::string serverIP) : IS_SERVER(isServer),
 		players.push_back(player);
 	}
 
-	GameObject* floor = new GameObject();
+	floor = new GameObject();
 	floor->setLocalScale(32.0f);
 	floor->setLocalPosition(glm::vec3(0, -32, 0));
 	floor->addRenderable();
