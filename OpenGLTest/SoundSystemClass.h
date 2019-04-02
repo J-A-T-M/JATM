20e5e944de070ae91b316cbf144ebe0df39bead8
limbermuffin <limbermuffin@gmail.com>
2019-04-02 16:39:43 -0700

    merged Yu's changes back in

---
+++ OpenGLTest/SoundSystemClass.h
@@ -0,0 +1,58 @@
+#include <FMOD/fmod.hpp>
+#include <FMOD/fmod_errors.h>  //only for error checking
+#include <iostream>
+
+typedef FMOD::Sound* SoundClass;
+
+class SoundSystemClass {
+	public:
+		// Pointer to the FMOD instance
+	FMOD::System *m_pSystem;
+
+	SoundSystemClass() {
+		if (FMOD::System_Create(&m_pSystem) != FMOD_OK) {
+			// Report Error
+			return;
+		}
+
+		int driverCount = 0;
+		m_pSystem->getNumDrivers(&driverCount);
+
+		if (driverCount == 0) {
+			// Report Error
+			return;
+		}
+
+		// Initialize our Instance with 36 Channels
+		m_pSystem->init(36, FMOD_INIT_NORMAL, nullptr);
+	}
+
+	void createSound(SoundClass *pSound, const char* pFile) {
+		FMOD_RESULT result = m_pSystem->createSound(pFile, FMOD_DEFAULT, 0, pSound);
+		FMODErrorCheck(result);
+	}
+
+	void playSound(SoundClass pSound, bool bLoop = false) {
+		if (!bLoop)
+			pSound->setMode(FMOD_LOOP_OFF);
+		else {
+			pSound->setMode(FMOD_LOOP_NORMAL);
+			pSound->setLoopCount(-1);
+		}
+
+		FMOD::Channel *channel;
+		m_pSystem->playSound(pSound, nullptr, false, &channel);
+	}
+
+	void releaseSound(SoundClass pSound) {
+		pSound->release();
+	}
+
+	private:
+	void FMODErrorCheck(FMOD_RESULT result) {
+		if (result != FMOD_OK) {
+			std::cout << "FMOD error! (" << result << ") " << FMOD_ErrorString(result) << std::endl;
+		}
+	}
+};
+
